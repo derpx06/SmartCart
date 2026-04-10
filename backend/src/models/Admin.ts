1731cbb5289e4218e86baf2e12ed1a1a5767dce9
Riddhesh Kataria <riddheshkataria@gmail.com>
2026-04-10 09:12:00 +0530

    Admin creds added

---
+++ backend/src/models/Admin.ts
@@ -0,0 +1,9 @@
+import { Schema, model } from 'mongoose';
+
+const adminSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+});
+
+export default model('Admin', adminSchema);
